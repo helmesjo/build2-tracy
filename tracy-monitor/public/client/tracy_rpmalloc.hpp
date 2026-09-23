f4390ba15e2d805948d794f03f10c572868798cc
helmesjo <helmesjo@gmail.com>
2026-09-23 17:11:32 +0200

    tracy-monitor: unbundle rpmalloc via librpmalloc
    Use the same path shims as libtracy-client.

--- tracy-monitor/public/client/tracy_rpmalloc.hpp
+++ tracy-monitor/public/client/tracy_rpmalloc.hpp
@@ -1,1 +1,31 @@
-../../../upstream/public/client/tracy_rpmalloc.hpp
+// Compatibility shim: Tracy sources include "tracy_rpmalloc.hpp" and use the
+// rpmalloc 1.4 API from namespace tracy (a namespaced copy of rpmalloc).
+// librpmalloc exports <rpmalloc.h> with the rpmalloc 2 API in the global
+// namespace, where initialization and thread finalization take different
+// arguments.
+//
+#pragma once
+
+#include <rpmalloc.h>
+
+namespace tracy
+{
+  using ::rpmalloc;
+  using ::rpfree;
+  using ::rprealloc;
+
+  using ::rpmalloc_finalize;
+  using ::rpmalloc_thread_initialize;
+
+  inline int
+  rpmalloc_initialize ()
+  {
+    return ::rpmalloc_initialize (nullptr);
+  }
+
+  inline void
+  rpmalloc_thread_finalize (int /* release_caches */)
+  {
+    ::rpmalloc_thread_finalize ();
+  }
+}
