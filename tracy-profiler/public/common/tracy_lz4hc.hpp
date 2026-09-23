86f62b82863d7c2937699274adb2c6a245da77a9
helmesjo <helmesjo@gmail.com>
2026-09-24 01:15:42 +0200

    tracy-profiler: unbundle lz4 via liblz4
    Replace the namespaced LZ4 1.9.4 copy (public/common/tracy_lz4{,hc}.
    {hpp,cpp}) with the same path shims tracy-tools uses, extended with
    LZ4_compressBound() and LZ4_compress_HC() for the embed helper.

--- tracy-profiler/public/common/tracy_lz4hc.hpp
+++ tracy-profiler/public/common/tracy_lz4hc.hpp
@@ -1,1 +1,21 @@
-../../../upstream/public/common/tracy_lz4hc.hpp
+// Compatibility shim: Tracy sources include "tracy_lz4hc.hpp" and use the
+// LZ4 HC API from namespace tracy (a namespaced copy of LZ4). liblz4
+// exports <lz4hc.h> with the same API in the global namespace.
+//
+#pragma once
+
+#include "tracy_lz4.hpp"
+
+#include <lz4hc.h>
+
+namespace tracy
+{
+  using ::LZ4_streamHC_t;
+
+  using ::LZ4_compress_HC;
+
+  using ::LZ4_createStreamHC;
+  using ::LZ4_freeStreamHC;
+  using ::LZ4_resetStreamHC;
+  using ::LZ4_compress_HC_continue;
+}
