0d4a1dd6f7d964ab042278aad788c0f0cc5c457f
helmesjo <helmesjo@gmail.com>
2026-09-23 16:55:18 +0200

    tracy-tools: unbundle getopt_port via libgetopt_port
    Replace the getopt_port copy (getopt/getopt.{h,c}) with a path shim that
    includes <getopt_port/getopt.h>. The packaged header is identical to the
    bundled one and its getopt.c differs by a cast only.

--- tracy-tools/getopt/getopt.c
+++
@@ -1,1 +0,0 @@
-../../upstream/getopt/getopt.c
--- tracy-tools/getopt/getopt.h
+++ tracy-tools/getopt/getopt.h
@@ -1,1 +1,6 @@
-../../upstream/getopt/getopt.h
+// Compatibility shim: Tracy sources include "../../getopt/getopt.h" (a copy
+// of getopt_port). libgetopt_port exports <getopt_port/getopt.h>.
+//
+#pragma once
+
+#include <getopt_port/getopt.h>
