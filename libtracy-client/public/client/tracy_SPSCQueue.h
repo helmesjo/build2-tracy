b22aebecf51bcdb814ca69bf7f72949364939bcc
helmesjo <helmesjo@gmail.com>
2026-09-23 11:44:09 +0200

    Unbundle SPSCQueue via libspscqueue
    Depend on libspscqueue ^1.1.0- from the local ../tracy-deps dir
    prerequisite. Replace the client/tracy_SPSCQueue.h symlink with a path
    shim that aliases tracy::SPSCQueue<T> to rigtorp::SPSCQueue<T> with an
    allocator forwarding to tracy_malloc()/tracy_free(), the only functional
    difference of the Tracy copy. The shim is reached from the public
    TracyProfiler.hpp, so libspscqueue is an interface dependency.

--- libtracy-client/public/client/tracy_SPSCQueue.h
+++ libtracy-client/public/client/tracy_SPSCQueue.h
@@ -1,1 +1,46 @@
-../../../upstream/public/client/tracy_SPSCQueue.h
+// Compatibility shim: Tracy sources include "tracy_SPSCQueue.h" and use
+// tracy::SPSCQueue<T>, a copy of rigtorp::SPSCQueue that allocates with
+// tracy_malloc()/tracy_free(). libspscqueue exports <rigtorp/SPSCQueue.h>.
+//
+#pragma once
+
+#include <cstddef>
+
+#include <rigtorp/SPSCQueue.h>
+
+#include "../common/TracyAlloc.hpp"
+
+namespace tracy
+{
+  template <typename T>
+  struct SPSCQueueAllocator
+  {
+    using value_type = T;
+
+    SPSCQueueAllocator () noexcept = default;
+
+    template <typename U>
+    SPSCQueueAllocator (const SPSCQueueAllocator<U>&) noexcept {}
+
+    T*
+    allocate (std::size_t n)
+    {
+      return static_cast<T*> (tracy_malloc (sizeof (T) * n));
+    }
+
+    void
+    deallocate (T* p, std::size_t) noexcept
+    {
+      tracy_free (p);
+    }
+
+    template <typename U>
+    bool operator== (const SPSCQueueAllocator<U>&) const noexcept {return true;}
+
+    template <typename U>
+    bool operator!= (const SPSCQueueAllocator<U>&) const noexcept {return false;}
+  };
+
+  template <typename T>
+  using SPSCQueue = rigtorp::SPSCQueue<T, SPSCQueueAllocator<T>>;
+}
